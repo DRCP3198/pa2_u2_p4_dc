5ffe0c8c2cf8b88a758ae076b616af6afaa42a98
Dillan Coloma <renatocoloma2010@gmail.com>
2023-06-09 18:18:35 -0500

    Base Datos taller 12

---
+++ src/main/java/com/example/demo/Pa2U2P4Dc1Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U2P4Dc1Application implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U2P4Dc1Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/example/demo/repository/Estudiante.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+public class Estudiante {
+	
+	private String nombre;
+	private String apellido;
+	private String cedula;
+
+}
